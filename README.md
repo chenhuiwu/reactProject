bded48100e9f4dbc3838a96f94f656719d015c98
chenhuiwu <xiaowucom.com@qq.com>
2018-12-11 11:28:08 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+1、安装了包antd axios redux react-redux react-router-dom less less-loader redux-actions redux-logger redux-thunk redux-promise 哪个包要是没有再安装一下
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+// console.log(ReactDOM)
+
+
+ReactDOM.render(
+    <div>一切从Hello，word！开始</div>
+,window.root)
